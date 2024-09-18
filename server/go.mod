1413b07808e6cc02a8cbf3e90e4fd57d95652b90
Monzón <leonardo.monzon@spaceguru.com>
2024-09-17 22:30:22 -0300

    leomonzondev

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/leomonzonspaceguru/fithub
 
 go 1.21.6
+
+require github.com/mattn/go-sqlite3 v1.14.23 // indirect
--- server/main.go
+++ server/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Contente-type","application/json")
+		response := map[string]string{"message":"Holaa"}
+		json.NewEncoder(w).Encode(response)
+	})
+
+	fmt.Println("Server runningg")
+	http.ListenAndServe(":5000",nil)
+
+}
